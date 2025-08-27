45b130f159727895f1cb978f47b8e53e878e75d2
Weickmam Ferreira <weickmam07@gmail.com>
2025-08-27 00:50:25 -0300

    feat: add credits and timer instructions to the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ export function Home() {
     <div className="bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 text-white min-h-screen w-full flex items-center justify-center px-4 py-2">
       <header className="absolute top-4 left-0 w-full flex justify-center pointer-events-none select-none">
         <span className="bg-stone-900/80 px-4 py-1 rounded-full text-xs text-stone-400 font-light shadow-sm pointer-events-auto">
+          idealizado por <a href="https://instagram.com/geovana_frade" target="_blank" className="underline font-semibold text-green-400">@geovana_frade</a> & 
           desenvolvido por <a href="https://instagram.com/weickfer" target="_blank" className="underline font-semibold text-green-400">@weickfer</a>
         </span>
       </header>
@@ -13,6 +14,9 @@ export function Home() {
         <h1 className="text-4xl font-extrabold tracking-tight mb-2 text-center text-green-400 drop-shadow-lg">
           Bem-vindo ao GeoTris!
         </h1>
+        <p className="text-stone-300 text-sm font-light tracking-wide text-center">
+            <span className="font-semibold text-white">Tempo Cronometrado:</span><br /> Resolva o máximo de fórmulas possível antes do tempo acabar!
+          </p>
         <h2 className="text-xl font-semibold text-stone-200 mb-1">Escolha o modo de jogo:</h2>
 
         <section className="w-full flex flex-col gap-3">
@@ -42,10 +46,7 @@ export function Home() {
           </div>
         </section>
 
-        <div className="mt-6 flex flex-col items-center gap-2">
-          <p className="text-stone-300 text-sm font-light tracking-wide text-center">
-            <span className="font-semibold text-white">Tempo Cronometrado:</span> Resolva o máximo de fórmulas possível antes do tempo acabar!
-          </p>
+        <div className="flex flex-col items-center gap-2">
           <Link
             to="/formulas"
             className="mt-2 text-xs text-blue-300 hover:underline hover:text-blue-400 transition"
